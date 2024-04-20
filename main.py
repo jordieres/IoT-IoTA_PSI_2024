c32c38b01bdad3c97fe4254c4afc70201ac0a139
irene <irene.peredaserrano@student.kuleuven.be>
2024-04-20 13:42:38 +0200

    Cambio de formato de impresión de los datos

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ from ruuvitag import core
 
 
 def cb(ruuvitag):
-    print(ruuvitag)
+    print("RuuviTagRAWv2(mac={}".format(ruuvitag.mac))
+    print("              rssi={}".format(ruuvitag.rssi))
+    print("              format={}".format(ruuvitag.format))
+    print("              humidity={}".format(ruuvitag.humidity))
+    print("              temperature={}".format(ruuvitag.temperature))
+    print("              pressure={}".format(ruuvitag.pressure))
+    print("              acceleration_x={}".format(ruuvitag.acceleration_x))
+    print("              acceleration_y={}".format(ruuvitag.acceleration_y))
+    print("              acceleration_z={}".format(ruuvitag.acceleration_z))
+    print("              battery_voltage={}".format(ruuvitag.battery_voltage))
+    print("              power_info={}".format(ruuvitag.power_info))
+    print("              movement_counter={}".format(ruuvitag.movement_counter))
+    print("              measurement_sequence={}".format(ruuvitag.measurement_sequence)+")")
     print("\n")
 
 
@@ -12,7 +24,7 @@ def run(ruuvi):
     try:
         while True:
             ruuvi.scan()
-            time.sleep_ms(50000)
+            time.sleep_ms(10000)
     except KeyboardInterrupt:
         ruuvi.stop()
 
